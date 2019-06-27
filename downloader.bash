4769824d8df0a59cd0d38dfca234fca1aa2ce533
Evergreen1213 <48633606+Evergreen1213@users.noreply.github.com>
2019-06-27 11:55:05 -0400

    Update downloader.bash
    stuff (mostly me trying to figure out how to use github desktop)

--- downloader.bash
+++ downloader.bash
@@ -3,7 +3,7 @@
 echo drag the folder where you want the file to download into this terminal window
 read download_folder
 cd $download_folder
-
+clear
 while true; do
     echo "which version would you like
     ╭───────────────────╮
@@ -12,23 +12,19 @@ while true; do
     ╰───────────────────╯"
     read version
     if [ $version = b ]; then
-        
-        rm -rfv "$update_file_directry"
         git clone -b "beta_version-(experimental)" https://github.com/Evergreen1213/the-miny-terminal-idk2.git
+
         break
     elif [ $version = m ]; then
-        rm -rfv "$update_file_directry"
         git clone https://github.com/Evergreen1213/the-miny-terminal-idk2.git
+        
         break
     else
         echo "choose an option please:"
+        clear
     fi
 done
-break
-
-
-echo "choose an option please:"
 
 
 
-#git clone https://github.com/Evergreen1213/the-miny-terminal-idk2.git; bash "/the-miny-terminal-idk2/download_and_run_the_github.bash"
+#git clone https://github.com/Evergreen1213/the_downloader.git; bash "/the_downloader/downloader.bash"
